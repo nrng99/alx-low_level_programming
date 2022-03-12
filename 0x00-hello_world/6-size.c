1072f9346fba153d0576756cd080797e275861ad
root <nrng99@gmail.com>
2022-03-12 06:55:37 -0800

    my commit on task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  *
- * main - Enter point
+ *main - Enter point
  *
- * Return: Always, 0 (Success)
+ *Return: Always, 0 (Success)
  */
 int main(void)
 { printf("Size of a char: %d byte(s)\n", sizeof(char));
-  printf("Size of an int: %d byte(s)\n", sizeof(int));
-  printf("size of a long int: %d byte(s)\n", sizeof(long int));
-  printf("size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a float: %d byte(s)\n", sizeof(float));
+printf("siz of a long long: %d byte(s)\n", sizeof(long long));
   return (0);
 }
 
