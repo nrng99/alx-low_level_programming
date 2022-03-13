48d4175df60f5178a5c5926b71ab0478b21e149b
root <nrng99@gmail.com>
2022-03-13 12:30:07 -0700

    my commit on task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,9 @@
 int main(void)
 
 {
-
 		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
+		srand(time(0));
+                n = rand() - RAND_MAX / 2;
 if (n==0)
 {
 	 printf("%i is zero\n", n); 
