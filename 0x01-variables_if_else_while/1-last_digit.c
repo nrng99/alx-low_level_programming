9609d4b9022f6cb6c29bca83d23defb2445ba712
root <nrng99@gmail.com>
2022-03-15 13:38:40 -0700

    my commit on task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/* more headers goes there 
+ * main - Entry point
+ * betty style doc for function main goes there */
 int main(void)
 {int n;
 srand(time(0));
