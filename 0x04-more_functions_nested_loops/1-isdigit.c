c6ef78601352a23b2246afdb9ab70da429539d8c
root <nrng99@gmail.com>
2022-03-19 04:27:30 -0700

    my commit on task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,14 @@
-#include "main.h"                                                                                                                       
-/**                                                                                                                                    
- *_isdigit - evalue if is digit.                                                                                             
- *@c: print int       
- *Return: Always 0.                                                                                                                     
- */                                                                                                                              
-int _isdigit(int c)                                                                                                                     
-{if (c >= 48 && c <= 57)                                                                                                              
-                                                                                                                                       
-return (1);                                                                                                                            
-                                                                                                                                       
-else                                                                                                                                                                                                                                                                           
-return (0);                                                                                                            
-                                                                                                                                       
+#include "main.h"
+/**
+ *_isdigit - check if c is digit between 0 to 9
+ *@c: character to be checked
+ *
+ *Return: 1 if c is digit , 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (0);
+else
+return (0);
 }
