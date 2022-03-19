ddc16e481582ffefdb3c2f019d43784db95ea8d3
root <nrng99@gmail.com>
2022-03-19 04:13:05 -0700

    my commit on task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,11 +7,10 @@
 int _isdigit(int c)                                                                                                                     
 {                                                                                                                                       
 if (c >= 48 && c <= 57)                                                                                                              
-{                                                                                                                                       
+                                                                                                                                       
 return (1);                                                                                                                            
-}                                                                                                                                       
-else                                                                                                                                    
-{                                                                                                                                       
-return (0);                                                                                                                             
-}                                                                                                                                       
+                                                                                                                                       
+else                                                                                                                                                                                                                                                                           
+return (0);                                                                                                            
+                                                                                                                                       
 }
